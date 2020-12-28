8bd7cf5c4fc853a9959913996357260fa6818356
Briac ferté <bferte@mbp-de-briac.home>
2020-12-28 13:55:08 +0100

    28/12/20

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
     {
@@ -46,6 +47,13 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 )
+
+//play function before save into display: 'block',
+userSchema.pre("save", async function(next) {
+    const salt = await bcrypt .genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
 
 const UserModel = mongoose.model('user', userSchema);
 
