961a92163105c137bccf7d6f0fb5698f7728c673
Briac ferté <bferte@mbp-de-briac.home>
2020-12-31 14:00:07 +0100

    31/12/20

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
 
 module.exports.signUp = async (req, res) => {
     const {pseudo, email, password} = req.body
@@ -9,5 +10,14 @@ module.exports.signUp = async (req, res) => {
     }
     catch(err) {
         res.status(200).send({ err })
+    }
+}
+
+module.exports.signIn = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await UserModel.login(email, password);
+        const token = createToken(user._id)
     }
 }
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -84,10 +84,29 @@ module.exports.follow = async (req, res) => {
 }
 
 module.exports.unfollow = async (req, res) => {
-    if (!ObjectID.isValid(req.params.id))
+    if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToUnfollow))
      return res.status(400).send('ID unknow : ' + req.params.id)
 
      try {
+        await UserModel.findByIdAndUpdate(
+            req.params.id,
+            { $pull: { following: req.body.idToUnfollow}},
+            { new: true, upsert: true },
+            (err, docs) => {
+                if (!err) res.status(201).json(docs);
+                else return res.status(400).json(err);
+            }
+        );
+        //add to following list
+        await UserModel.findByIdAndUpdate(
+            req.body.idToUnfollow,
+            { $pull: { followers: req.params.id }},
+            { new: true, upsert: true },
+            (err, docs) => {
+                //if (!err) res.status(201).json(docs);
+               if (err) return res.status(400).json(err);
+            }
+        )
 
      } catch (err) {
         return res.status(500).json({ message: err});
